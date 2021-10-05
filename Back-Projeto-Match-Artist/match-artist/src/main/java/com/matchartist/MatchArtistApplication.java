9b68867231cc0a29cc920e76bcb1ded7fb03a6c2
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-10-04 22:39:38 -0300

    adicionado back-end

---
+++ Back-Projeto-Match-Artist/match-artist/src/main/java/com/matchartist/MatchArtistApplication.java
@@ -0,0 +1,13 @@
+package com.matchartist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchArtistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchArtistApplication.class, args);
+	}
+
+}
---
+++ Back-Projeto-Match-Artist/match-artist/src/test/java/com/matchartist/MatchArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchartist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchArtistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
