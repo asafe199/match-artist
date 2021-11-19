153bfd6cc8bc4f7bcc5cbc41d173c3392d0d6362
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-19 18:59:38 -0300

    create usuario

--- back-end/src/main/java/com/matchartist/backend/resource/UsuarioResource.java
+++ back-end/src/main/java/com/matchartist/backend/resource/UsuarioResource.java
@@ -3,11 +3,15 @@ package com.matchartist.backend.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.matchartist.backend.repository.UsuarioRepository;
-
+import com.matchartist.backend.model.TipoUsuario;
 import com.matchartist.backend.model.Usuario;
 
 
@@ -21,6 +25,12 @@ public class UsuarioResource {
 	@GetMapping("/usuario")
 	public  List<Usuario> listar(){
 		return UsuarioRepository.findAll();
+	}
+	
+	@PostMapping("/usuario")
+	@ResponseStatus(HttpStatus.CREATED)
+	public  void create (@RequestBody Usuario usuario){
+		UsuarioRepository.save(usuario);
 	}
 
 }
