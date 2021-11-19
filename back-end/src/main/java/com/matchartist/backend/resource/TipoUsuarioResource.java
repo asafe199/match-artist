a8c68736d3044eb3a586e984678e1567261661cc
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-19 18:45:10 -0300

    create tipoUsuario

--- back-end/src/main/java/com/matchartist/backend/resource/TipoUsuarioResource.java
+++ back-end/src/main/java/com/matchartist/backend/resource/TipoUsuarioResource.java
@@ -3,8 +3,12 @@ package com.matchartist.backend.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matchartist.backend.model.TipoUsuario;
@@ -20,6 +24,12 @@ public class TipoUsuarioResource {
 	@GetMapping("/tipoUsuario")
 	public  List<TipoUsuario> listar(){
 		return tipoUsuarioRepository.findAll();
+	}
+	
+	@PostMapping("/tipoUsuario")
+	@ResponseStatus(HttpStatus.CREATED)
+	public  void create (@RequestBody TipoUsuario tipoUsuario){
+		tipoUsuarioRepository.save(tipoUsuario);
 	}
 
 	
