13fc9dd813e1818461d12d75d399c2690be3e434
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-26 18:35:27 -0300

    modificado usuarioResource para retornar o usuario cadastrado e o locationm

--- back-end/src/main/java/com/matchartist/backend/resource/UsuarioResource.java
+++ back-end/src/main/java/com/matchartist/backend/resource/UsuarioResource.java
@@ -1,16 +1,23 @@
 package com.matchartist.backend.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.matchartist.backend.repository.UsuarioRepository;
+import com.matchartist.backend.model.Endereco;
 import com.matchartist.backend.model.TipoUsuario;
 import com.matchartist.backend.model.Usuario;
 
@@ -20,17 +27,23 @@ import com.matchartist.backend.model.Usuario;
 public class UsuarioResource {
 	
 	@Autowired
-	private UsuarioRepository UsuarioRepository;
+	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping("/usuario")
 	public  List<Usuario> listar(){
-		return UsuarioRepository.findAll();
+		return usuarioRepository.findAll();
 	}
 	
+	
 	@PostMapping("/usuario")
-	@ResponseStatus(HttpStatus.CREATED)
-	public  void create (@RequestBody Usuario usuario){
-		UsuarioRepository.save(usuario);
+	public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario, HttpServletResponse response) {
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(usuarioSalvo.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(usuarioSalvo);
 	}
 
 }
