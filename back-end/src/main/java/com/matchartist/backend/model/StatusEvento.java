e7a367ff8b05cdca978f02391deca6de1722a2ae
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-26 19:08:37 -0300

    Adicionado model StatusEvento

---
+++ back-end/src/main/java/com/matchartist/backend/model/StatusEvento.java
@@ -0,0 +1,39 @@
+package com.matchartist.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "status_evento")
+public class StatusEvento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="id")
+	private int id;
+	
+	@Column(name = "name_status_evento")
+	private String nameStatusEvento;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNameStatusEvento() {
+		return nameStatusEvento;
+	}
+
+	public void setNameStatusEvento(String nameStatusEvento) {
+		this.nameStatusEvento = nameStatusEvento;
+	}
+	
+	
+
+}
