456d2159ba1e6eddcd90260671547b3d06b9ba13
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-28 23:19:33 -0300

    adicionado Config inicial spriong security

---
+++ back-end/src/main/java/com/matchartist/backend/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.matchartist.backend.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//	quais rotas estaram publicas
+	private static final String[] PUBLIC_MATCHERS = {
+//		"/tipoUsuario/**"
+	};
+	
+	//apenas leitura
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/tipoUsuario/**"
+		};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();			 // para todo resto exige autenticação
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean	
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+	}	
+
+}
