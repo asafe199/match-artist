d5d2b590c70b187e36ee5b73e0bd3d701e39e81b
Gabriel Anadinho Pinheiro <gabrielanadinho@gmail.com>
2021-11-30 22:34:33 -0300

    adicionado dados na tabela statusConta e tipoUsuario

---
+++ back-end/src/main/resources/db/migration/V04__insere_dados_statusConta_tipoUsuario.sql
@@ -0,0 +1,16 @@
+--  Artista
+INSERT INTO match_artist2.tipo_usuario (id,name_tipo_usuario)
+	VALUES (1,'Artista');
+INSERT INTO match_artist2.tipo_usuario (id,name_tipo_usuario)
+	VALUES (2,'Estabelecimento');
+	
+INSERT INTO match_artist2.tipo_usuario (id,name_tipo_usuario)
+	VALUES (3,'Adm');
+
+-- statusConta
+--  Auto-generated SQL script #202111302232
+INSERT INTO match_artist2.status_conta (id,name)
+	VALUES (1,'Ativada');
+	
+INSERT INTO match_artist2.status_conta (id,name)
+	VALUES (2,'Desativada');
